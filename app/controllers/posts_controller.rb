a974421fb42ebdfeb0b5501af75903d98c41e837
jolozy <joannelozy@outlook.com>
2016-03-21 00:34:16 +0800

    Completes Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,13 +11,43 @@ class PostsController < ApplicationController
   end
   #create: clicking of upload button to allow creation of new entry. Redirects to new post
   def create
-    @post = Post.create(post_params)
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+    flash.now[:alert] = "Your new post could not be created!"
+    render :new
+    end
+  end
+
+  #/posts/id shows individual post
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  #/posts/id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+  #clicking button to update: actually perform an update that touches our database. It’s very similar to our create action but rather than creating, we’re UPDATING
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash[:success] = "Your post has been updated!"
+    redirect_to(post_path(@post))
+  end
+
+  #deleting a post
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = "Your post has been deleted!"
     redirect_to posts_path
   end
 
 
-
   private
+
   def post_params
     params.require(:post).permit(:image, :title, :description, :budget)
   end
